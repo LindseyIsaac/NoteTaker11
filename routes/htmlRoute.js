38bc5a1f2306f4bfc00aab5f8de3f51494e2ee37
LindseyIsaac <isaaclindsey89@gmail.com>
2023-05-30 12:28:48 -0500

    figuring where to start

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,3 +1,20 @@
 const path = require('path');
 
 // Need my get here and a link to my index and notes
+
+
+// example
+// const path = require('path');
+// const router = require('express').Router();
+
+// // "/notes" responds with the notes.html file
+// router.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/notes.html'));
+// });
+
+// // All other routes respond with the index.html file
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
+
+// module.exports = router;
